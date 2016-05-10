c314858b90916ee5901f519a23ef57be632e8c02
Elias <elias0789@gmail.com>
2016-05-10 15:13:34 +0100

    Try to get material ui working

--- frontend/src/js/components/Header/header_index.js
+++ frontend/src/js/components/Header/header_index.js
@@ -1,42 +1,14 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, NavItem } from 'react-materialize'
 import { Link } from 'react-router'
 
 export default class Header extends React.Component {
-  constructor () {
-    super()
-    this.state = { menuOpen: false }
-  }
 
   render () {
     return (
       <div>
-        <Navbar
-          expanded={this.state.menuOpen}
-          onToggle={() => { this.setState({ menuOpen: !this.state.menuOpen }) }}
-          className='top-menu'
-          fixedTop={true}>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to={'/'}>
-                <img src={this.props.logoUrl}></img>
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse pullRight>
-            <Nav pullRight>
-              {this.props.menuItems.map(item => {
-                return (
-                  <li onClick={() => { this.setState({ menuOpen: false }) }}
-                    role='presentation'
-                    key={item + '-li'}>
-                    <Link key={item} to={'/' + item}>{item}</Link>
-                  </li>
-                )
-              })}
-            </Nav>
-          </Navbar.Collapse>
+        <Navbar brand='logo' right>
+          <NavItem><Link to='/homeless'>Get Started</Link></NavItem>
         </Navbar>
       </div>
     )
