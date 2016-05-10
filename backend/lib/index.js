05e814af65ef38f085a757b8fbd517c333313ec0
Elias <elias0789@gmail.com>
2016-05-10 18:55:53 +0100

    Without redis

--- backend/lib/index.js
+++ backend/lib/index.js
@@ -1,13 +1,13 @@
 require('env2')('./config.env')
 const createServer = require('./server.js')
-const createClient = require('./redis/client.js')
+// const createClient = require('./redis/client.js')
 
 // with redis
-const client = createClient()
-const server = createServer(client)
+// const client = createClient()
+// const server = createServer(client)
 
 // without redis
-// const server = createServer()
+const server = createServer()
 
 server.start((err) => {
   if (err) {
